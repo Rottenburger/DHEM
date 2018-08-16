2cd2e2756fd52ae5f8115e7e85738a7bd11b55a9
Rottenburger <40438324+Rottenburger@users.noreply.github.com>
2018-08-16 15:10:14 +0100

    Added new inputs to name the text file

--- tews.py
+++ tews.py
@@ -1,7 +1,8 @@
+textname = input("What is the Name?")
 eventid = input("What is the event id?")
 country = input("What is the Country?")
 
-file = open('eng.txt', 'w')
+file = open(textname + '.txt', 'w')
 file.write("event = {\n")
 file.write("	id = " + eventid + "\n")
 file.write("	country = " + country + "\n")
